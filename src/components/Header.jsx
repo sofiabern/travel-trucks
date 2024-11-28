734d1d507cfa1e337f6f0ec71801f0eb4a7bd78a
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-11-28 07:36:17 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
+import styled from "styled-components";
 import Navigation from "./Navigation";
+import Logo from "./Logo";
+
+const StyledHeader = styled.header`
+  background-color: var(--background-lighter);
+  padding: 20px 64px 19px;
+  border-bottom: 1px solid var(--background-light);
+  position: relative;
+`;
+
 const Header = () => {
-    return (
-        <Navigation />
-    );
-  };
-  
-  export default Header;
+  return (
+    <StyledHeader>
+      <Logo />
+      <Navigation />
+    </StyledHeader>
+  );
+};
+
+export default Header;
