341e1fde890c3cb84c2df59ffe470a43ad9f9b9b
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-11-28 07:36:03 +0200

    navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,31 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+  display: flex;
+  gap: 24px;
+  justify-content: center;
+`;
+
+const StyledLink = styled(NavLink)`
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 1.5;
+  padding: 4px;
+  transition: color var(--func-time) var(--func);
+
+  &.active,
+  &:hover {
+    color: var(--red);
+  }
+`;
+
 const Navigation = () => {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/catalog">Catalog</Link>
-    </nav>
+    <StyledNav>
+      <StyledLink to="/">Home</StyledLink>
+      <StyledLink to="/catalog">Catalog</StyledLink>
+    </StyledNav>
   );
 };
 
