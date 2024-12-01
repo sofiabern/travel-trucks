6a35763bc7d1e823ab6f665a56de03767568b005
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-01 11:42:07 +0200

    data for card labels

---
+++ src/data/features.js
@@ -0,0 +1,13 @@
+const featureList = [
+    { key: "AC", iconId: "wind", label: "AC" },
+    { key: "kitchen", iconId: "cup", label: "Kitchen" },
+    { key: "TV", iconId: "tv", label: "TV" },
+    { key: "radio", iconId: "radio", label: "Radio" },
+    { key: "bathroom", iconId: "shower", label: "Bathroom" },
+    { key: "refrigerator", iconId: "fridge", label: "Refrigerator" },
+    { key: "microwave", iconId: "microwave", label: "Microwave" },
+    { key: "gas", iconId: "stove", label: "Gas" },
+    { key: "water", iconId: "water", label: "Water" },
+   
+  ];
+  export default featureList;
