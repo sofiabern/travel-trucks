dbb6dacb226eb2cfea1edab105e2516cdb550ba5
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-01 11:47:14 +0200

    updated FilterTitle

---
+++ src/components/FilterTitle.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const FilterTitle = styled.h3`
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 1.2;
+  color: var(--dark-blue);
+  margin-bottom: 49px;
+  }
+`;
+
+export default FilterTitle;
