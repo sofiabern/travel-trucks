24c3c2c6d6a6172fa144063347aea6a8130c1beb
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-01 22:40:46 +0200

    created Truck page

---
+++ src/pages/TruckPage/TruckPage.styles.js
@@ -0,0 +1,13 @@
+export const Name = styled.h1`
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 1.3;
+  color: var(--dark-blue);
+`;
+
+export const Price = styled.h2`
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 1.3;
+  color: var(--dark-blue);
+`;
